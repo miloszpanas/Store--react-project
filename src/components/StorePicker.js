ef83a262dec96c7fd127ad715ab02de4e4e49573
Miłosz Panas <miloszpanas@gmail.com>
2018-10-24 17:05:45 +0200

    Update StorePicker component, add routing to whatever is entered

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -2,12 +2,24 @@ import React from "react";
 import { getFunName } from "../helpers";
 
 class StorePicker extends React.Component {
+  myInput = React.createRef();
+
+  goToStore = event => {
+    // 1. Stop the form from submitting
+    event.preventDefault();
+    //  2. get the text from that input
+    const storeName = this.myInput.value.value;
+    // 3. Change the page to /store/whatever-entered
+    this.props.history.push(`/store/${storeName}`);
+  };
+
   render() {
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={this.goToStore}>
         <h2>Please enter a Store</h2>
         <input
           type="text"
+          ref={this.myInput}
           required
           placeholder="Store Name"
           defaultValue={getFunName()}
