7813cc78a1609b919fc0e2880d83061d4bd8e659
Miłosz Panas <miloszpanas@gmail.com>
2018-10-25 18:01:30 +0200

    Implement order compone logic to display item and count the price

--- src/components/App.js
+++ src/components/App.js
@@ -26,6 +26,17 @@ class App extends React.Component {
     this.setState({ fishes: sampleFishes });
   };
 
+  addToOrder = key => {
+    // 1. take a copy of state
+    const order = { ...this.state.order };
+    // 2. either add to the order or update the order
+    order[key] = order[key] + 1 || 1;
+    // 3. Call setState to uodate the state object
+    this.setState({
+      order: order
+    });
+  };
+
   render() {
     return (
       <div className="catch-of-the-day">
@@ -33,11 +44,16 @@ class App extends React.Component {
           <Header tagline="Fresh Seafood Market" />
           <ul className="fishes">
             {Object.keys(this.state.fishes).map(key => (
-              <Fish key={key} details={this.state.fishes[key]} />
+              <Fish
+                key={key}
+                index={key}
+                details={this.state.fishes[key]}
+                addToOrder={this.addToOrder}
+              />
             ))}
           </ul>
         </div>
-        <Order />
+        <Order fishes={this.state.fishes} order={this.state.order} />
         <Inventory
           addFish={this.addFish}
           loadSampleFishes={this.loadSampleFishes}
--- src/components/Fish.js
+++ src/components/Fish.js
@@ -2,8 +2,13 @@ import React from "react";
 import { formatPrice } from "../helpers";
 
 class Fish extends React.Component {
+  handleClick = () => {
+    this.props.addToOrder(this.props.index);
+  };
+
   render() {
     const { image, name, price, desc, status } = this.props.details;
+    const isavailable = status === "available";
 
     return (
       <li className="menu-fish">
@@ -13,7 +18,9 @@ class Fish extends React.Component {
           <span className="price">{formatPrice(price)}</span>
         </h3>
         <p>{desc}</p>
-        <button>Add To Cart</button>
+        <button disabled={!isavailable} onClick={this.handleClick}>
+          {isavailable ? "Add to Order" : "Sold out!"}
+        </button>
       </li>
     );
   }
